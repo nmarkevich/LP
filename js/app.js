2fe85e24aedaf2991873847747075829890dc786
Natallia Markevich <natallia.markevich@gmail.com>
2021-04-18 18:50:58 -0500

    refactor: Moving hiding navbar function

--- js/app.js
+++ js/app.js
@@ -2,12 +2,13 @@
 const menuItems = document.querySelectorAll("section");
 const navBarList = document.querySelector("#navbar__list");
 
-//Navbar menu creation
 for (const menuItem of menuItems) {
+    //Navbar menu creation
     const listItem = document.createElement("li");
     const listLink = document.createElement("a");
     listLink.textContent = menuItem.getAttribute("data-nav");
     listLink.setAttribute("href", "#" + menuItem.getAttribute("id"));
+    listLink.setAttribute("class", "menu__link");
     listItem.appendChild(listLink);
     navBarList.appendChild(listItem);
     //Making the sections collapsible
@@ -19,15 +20,17 @@ for (const menuItem of menuItems) {
     //Expand the section when a user navigates to the section through the menu
     listLink.addEventListener("click" , ( ) => {
         menuItem.setAttribute("class","your-active-class");
-    });
-    //Hiding navbar when a user doesn't scroll
-    window.addEventListener("scroll", () => {
-        navBarList.style.display = "block";
-        setTimeout(() => {
-            navBarList.style.display = "none";
-        }, 3000);
+        menuItem.style.backgroundColor = "gray";
     });
 }
+
+//Hiding navbar when a user doesn't scroll
+window.addEventListener("scroll", () => {
+    navBarList.style.display = "block";
+    setTimeout(() => {
+        navBarList.style.display = "none";
+    }, 3000);
+});
 
 const myButton = document.getElementById("buttonToTop");
 
@@ -46,58 +49,6 @@ myButton.addEventListener("click", topFunction);
 function topFunction () {
     document.documentElement.scrollTo(0,0);
 }
-
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-
-
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
-
-
-
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
-
-// build the nav
-
-
-// Add class 'active' to section when near top of viewport
-
-
-// Scroll to anchor ID using scrollTO event
-
-
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
 
