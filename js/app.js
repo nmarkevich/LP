affb87b694565ee7b9756474d1063440df89c5e2
Natallia Markevich <natallia.markevich@gmail.com>
2021-04-19 20:53:52 -0500

    refactor: Combining  2 scroll listeners

--- js/app.js
+++ js/app.js
@@ -15,15 +15,7 @@ for (const menuItem of sectionItems) {
     //Making the sections collapsible
     menuItem.addEventListener("click" , ( ) => {
         menuItem.classList.toggle("collapsible");
-        /*menuItem.style.backgroundColor = "gray";
-        listItem.setAttribute("class", "active");*/
     });
-    /*//Expand the section when a user navigates to the section through the menu
-    listLink.addEventListener("click" , ( ) => {
-        menuItem.setAttribute("class","your-active-class");
-        menuItem.style.backgroundColor = "gray";
-        listItem.setAttribute("class", "active");
-    });*/
 }
 
 //Hiding navbar when a user doesn't scroll
@@ -36,21 +28,10 @@ window.addEventListener("scroll", () => {
 
 const myButton = document.getElementById("buttonToTop");
 
-//Showing 'Go to top' button when a user scrolls to the fold of the page
-window.onscroll = () => {
-    if (document.documentElement.scrollTop+window.innerHeight+10 >= document.documentElement.scrollHeight) {
-        myButton.style.display = "block";
-    } else {
-        myButton.style.display = "none";
-    }
-};
-
 //Navigating a user to the top of the page when 'Go to top' button is clicked.
-myButton.addEventListener("click", topFunction);
-
-function topFunction () {
+myButton.addEventListener("click", () => {
     document.documentElement.scrollTo(0,0);
-}
+});
 
 window.addEventListener("scroll", () => {
     //Making section active when it's in the viewport
@@ -64,6 +45,12 @@ window.addEventListener("scroll", () => {
             sectionItem.classList.remove("your-active-class");
             menuItem.classList.remove("active");
         }
+    }
+
+    if (document.documentElement.scrollTop+window.innerHeight+10 >= document.documentElement.scrollHeight) {
+        myButton.style.display = "block";
+    } else {
+        myButton.style.display = "none";
     }
 });
 
